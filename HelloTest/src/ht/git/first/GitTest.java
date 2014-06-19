2f5acff73aa72aa8a11c8fba7b08665358c79205
saket <saketkumaryadav@gmail.com>
2014-06-19 12:12:55 +0530

    Signed-off-by: saket <saketkumaryadav@gmail.com>

--- HelloTest/src/ht/git/first/GitTest.java
+++ HelloTest/src/ht/git/first/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 System.out.println("Git is fun");
+		 System.out.println("Git is test view");
 		 
 		 for (int i = 0; i < args.length; i++) {
 			System.out.println("[i]:"+i);
