47538ac7c23ba904dbd7bbc847a23b90f96d148d
saket <saketkumaryadav@gmail.com>
2014-06-19 14:07:30 +0530

    create new Method getName()

--- HelloTest/src/ht/git/first/GitTest.java
+++ HelloTest/src/ht/git/first/GitTest.java
@@ -2,6 +2,7 @@ package ht.git.first;
 
 public class GitTest {
 
+	private String name=null;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("Git is test view");
@@ -9,6 +10,14 @@ public class GitTest {
 		 for (int i = 0; i < args.length; i++) {
 			System.out.println("[i]:"+i);
 		}
+		 
+		GitTest gt=new GitTest();
+		System.out.println(gt.getName());
+	}
+	
+	public String getName(){
+		name="Admin";
+		return name;
 	}
 
 }
