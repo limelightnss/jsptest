4010320a5cdaa18286a65d3b3417e303192afb4d
saket <saketkumaryadav@gmail.com>
2014-06-19 12:07:05 +0530

    Set for loop in GitTest.java file

--- HelloTest/src/ht/git/first/GitTest.java
+++ HelloTest/src/ht/git/first/GitTest.java
@@ -5,6 +5,10 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("Git is fun");
+		 
+		 for (int i = 0; i < args.length; i++) {
+			System.out.println("[i]:"+i);
+		}
 	}
 
 }
